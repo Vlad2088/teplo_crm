97284e125e14bac0d8dab4b65af249e268f9e725
Vlad2088 <vladislavganieff@yandex.ru>
2026-09-01 03:36:24 +0000

    fix: documents.description (миграция) — поле было в форме, но отсутствовало в БД

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_01_032000) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_01_034000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_01_032000) do
   create_table "documents", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
+    t.text "description"
     t.integer "doc_type"
     t.date "document_date"
     t.string "file_path"
